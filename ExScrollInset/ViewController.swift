c9624404032547063f52406cf33aec24b9654bf5
jake <palatable7@naver.com>
2023-01-31 22:43:10 +0900

    Implement

---
+++ ExScrollInset/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ExScrollInset
+//
+//  Created by 김종권 on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
